ca76b9ef41d023c7aa5087811e2b486b8c175e4b
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2019-01-09 23:18:22 +0800

    Updated location

--- BelizeCityTour/ProjectIntroductionPage/ProjectIntroductionView.swift
+++ BelizeCityTour/ProjectIntroductionPage/ProjectIntroductionView.swift
@@ -42,7 +42,7 @@ class ProjectIntroductionView: UIView {
         return label
     }()
     
-    lazy var leftButton: UIButton = {
+    lazy var forwardButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.addTarget(self, action: #selector(goToLastCell(sender:)), for: .touchUpInside)
         btn.setImage(UIImage(named: "btn_back")?.withRenderingMode(.alwaysOriginal), for: .normal)
@@ -55,7 +55,7 @@ class ProjectIntroductionView: UIView {
         collectionView.scrollToLastCell()
     }
     
-    lazy var rightButton: UIButton = {
+    lazy var nextButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.addTarget(self, action: #selector(goToNextCell(sender:)), for: .touchUpInside)
         btn.setImage(UIImage(named: "btn_next")?.withRenderingMode(.alwaysOriginal), for: .normal)
@@ -87,8 +87,8 @@ class ProjectIntroductionView: UIView {
         backgroundColor = UIColor.backgroundRiceColor.withAlphaComponent(0.7)
         addSubview(backgroundImgView)
         addSubview(titleLabel)
-        addSubview(leftButton)
-        addSubview(rightButton)
+        addSubview(forwardButton)
+        addSubview(nextButton)
         addSubview(collectionView)
         backgroundImgView.fullAnchor(superView: self)
         addSubview(titleLabel)
@@ -104,13 +104,13 @@ class ProjectIntroductionView: UIView {
         hocButton.anchor(top: collectionView.bottomAnchor, bottom: nil, left: nil, right: collectionView.rightAnchor, topPadding: 10, bottomPadding: 0, leftPadding: 0, rightPadding: 20, width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.width / 6)
         
         
-        addSubview(leftButton)
-        leftButton.anchor(top: nil, bottom: nil, left: leftAnchor, right: nil, topPadding: 0, bottomPadding: 0, leftPadding: 30, rightPadding: 0, width: 50, height: 50)
-        leftButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        addSubview(forwardButton)
+        forwardButton.anchor(top: nil, bottom: nil, left: leftAnchor, right: nil, topPadding: 0, bottomPadding: 0, leftPadding: 30, rightPadding: 0, width: 50, height: 50)
+        forwardButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
-        addSubview(rightButton)
-        rightButton.anchor(top: nil, bottom: nil, left: nil, right: rightAnchor, topPadding: 0, bottomPadding: 0, leftPadding: 0, rightPadding: 30, width: 50, height: 50)
-        rightButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        addSubview(nextButton)
+        nextButton.anchor(top: nil, bottom: nil, left: nil, right: rightAnchor, topPadding: 0, bottomPadding: 0, leftPadding: 0, rightPadding: 30, width: 50, height: 50)
+        nextButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
     init(projectIntroduction: ProjectIntroduction) {
@@ -134,12 +134,12 @@ extension ProjectIntroductionView: ProjectCollectionViewDelegate{
             self?.backgroundImgView.alpha = 0.1
             
             if currentIndex.item == 0{
-                self?.leftButton.alpha = 0.0
+                self?.forwardButton.alpha = 0.0
             }else if currentIndex.item == projectIntroductions.count - 1{
-                self?.rightButton.alpha = 0.0
+                self?.nextButton.alpha = 0.0
             }else{
-                self?.leftButton.alpha = 1.0
-                self?.rightButton.alpha = 1.0
+                self?.forwardButton.alpha = 1.0
+                self?.nextButton.alpha = 1.0
             }
             
         }) { (_) in
